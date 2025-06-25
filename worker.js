c4a4f0fd4a8967175c7812865efa6733edf95c9a
sanchit-bidgely <sanchit@bidgely.com>
2025-06-25 13:17:12 +0530

    initi

---
+++ worker.js
@@ -0,0 +1,12 @@
+import puppeteer from "@cloudflare/puppeteer";
+
+export default {
+  async fetch(request, env) {
+    const browser = await puppeteer.launch(env.MYBROWSER, { keep_alive: 60_000 });
+    const page = await browser.newPage();
+    await page.goto("https://example.com");
+    const metrics = await page.metrics();
+    // await browser.close();
+    return Response.json(metrics);
+  },
+};
